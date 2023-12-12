4cc89f8ff7023bc806887913675383664933c7ef
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2023-12-12 17:22:47 +0900

    [level 2] Title: 귤 고르기, Time: 64.27 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,33 +1,14 @@
 import java.util.*;
 import java.io.*;
+
 class Solution {
     public int solution(int k, int[] tangerine) {
-        int answer = 1;
+        int answer = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<tangerine.length; i++){
             map.put(tangerine[i], map.getOrDefault(tangerine[i],0)+1);
-        } 
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int key : map.keySet() ){
-            // System.out.println(map.get(key));
-            list.add(map.get(key));
         }
-        Collections.sort(list, new Comparator<Integer>(){
-            @Override
-            public int compare(Integer o1, Integer o2){
-                return o2-o1;
-            }
-        });
-        // System.out.println(list.toString());
-        int sum = 0;
-        for(int i=0; i<list.size(); i++){
-            sum += list.get(i); 
-            if(sum>=k){
-                
-                break;
-            } answer++;
-        }
-        
+        System.out.println(map.size());
         return answer;
     }
 }
