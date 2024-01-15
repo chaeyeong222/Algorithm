dbd409842ef204b4aea50b969e38579c32e6b752
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2024-01-15 15:00:38 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1840 ms, Memory: 99320 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -1,50 +1,53 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken()); //정점수
-        int m = Integer.parseInt(st.nextToken()); //간선수 
-        int r = Integer.parseInt(st.nextToken());
-        int idx = 1;
-        PriorityQueue<Integer>[] list = new PriorityQueue[n+1];
-        for (int i = 1; i < list.length; i++) {
-            list[i] = new PriorityQueue<>();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        List<Integer>[] list = new ArrayList[N+1];
+        for (int i = 0; i < N+1; i++) {
+            list[i] = new ArrayList<>();
         }
-        int[] visited = new int[n+1];
-        
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int num1 = Integer.parseInt(st.nextToken());
             int num2 = Integer.parseInt(st.nextToken());
             list[num1].add(num2);
             list[num2].add(num1);
         }
-//        for (int i = 1; i <= n; i++) {
-//            Collections.sort(list[i]);
-//        }
+        for (int i = 0; i < N+1; i++) {
+            Collections.sort(list[i]);
+        }
+
+        int[] visited = new int[N+1];
+        Arrays.fill(visited,0);
+
+        int turn = 1;
+
         Queue<Integer> que = new LinkedList<>();
-        que.offer(r);
-        visited[r] = idx++;
+        que.offer(R);
+        visited[R]=turn++;
         while(!que.isEmpty()){
             int check = que.poll();
-            while(!list[check].isEmpty()){
-                int aaa = list[check].poll();
-                if(visited[aaa]==0){
-                    que.offer(aaa);
-                    visited[aaa] = idx++;
+            for(int num : list[check]){
+                if(visited[num]==0){
+                    visited[num]=turn++;
+                    que.offer(num);
                 }
             }
         }
-        for (int i = 1; i <= n; i++) {
+        for(int i=1; i<visited.length; i++){
             System.out.println(visited[i]);
         }
 
 
     }
-
 }
