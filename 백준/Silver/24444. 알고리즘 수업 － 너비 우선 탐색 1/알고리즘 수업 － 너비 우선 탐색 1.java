5fd203e3675fa647c0b2c059baea9962a0f5119c
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2024-01-16 10:10:41 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1828 ms, Memory: 97444 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.sql.Array;
 import java.util.*;
 
 public class Main {
@@ -13,9 +12,10 @@ public class Main {
         int R = Integer.parseInt(st.nextToken());
 
         List<Integer>[] list = new ArrayList[N+1];
-        for (int i = 0; i < N+1; i++) {
+        for (int i = 1; i <= N; i++) {
             list[i] = new ArrayList<>();
         }
+        //입력받은 값 넣기
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int num1 = Integer.parseInt(st.nextToken());
@@ -23,31 +23,29 @@ public class Main {
             list[num1].add(num2);
             list[num2].add(num1);
         }
-        for (int i = 0; i < N+1; i++) {
+        //오름차순정리
+        for (int i = 1; i <= N; i++) {
             Collections.sort(list[i]);
         }
-
+        
+        //방문순서 체크
         int[] visited = new int[N+1];
-        Arrays.fill(visited,0);
-
         int turn = 1;
-
+        visited[R] = turn++;
         Queue<Integer> que = new LinkedList<>();
         que.offer(R);
-        visited[R]=turn++;
         while(!que.isEmpty()){
-            int check = que.poll();
-            for(int num : list[check]){
-                if(visited[num]==0){
-                    visited[num]=turn++;
-                    que.offer(num);
+            int now = que.poll();
+            for (int nn : list[now]){
+                if(visited[nn]==0){//방문안했다면
+                    visited[nn]=turn++;
+                    que.offer(nn);
                 }
             }
         }
-        for(int i=1; i<visited.length; i++){
+
+        for (int i = 1; i <= N; i++) {
             System.out.println(visited[i]);
-        }
-
-
+        } 
     }
 }
