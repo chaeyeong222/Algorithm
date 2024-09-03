6d87fd568d43f51a355edb6556f6084948c0eca4
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2024-09-03 11:22:04 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 96 ms, Memory: 16484 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+//#숨바꼭질
+public class Main {
+    static int sis;
+    static int cnt;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int me = Integer.parseInt(st.nextToken());
+        sis = Integer.parseInt(st.nextToken());
+        cnt =  Integer.MAX_VALUE;
+        findSis(me,0);
+        System.out.println(cnt);
+    }
+
+    public static void findSis(int nowSpot, int time){
+        boolean[] visited = new boolean[100001];
+        Queue<int[]> que = new LinkedList<>();//0현재위치, 1횟수
+        que.offer(new int[]{nowSpot, time});
+        visited[nowSpot] = true;
+        while(!que.isEmpty()){
+            int[] now = que.poll();
+            if(now[0]==sis) {
+                cnt = Math.min(now[1], cnt);
+            }
+            if(now[0]*2 <=100000 && !visited[now[0]*2]) {
+                visited[now[0]*2]= true;
+                que.offer(new int[]{now[0]*2, now[1]});
+            }
+            if(now[0]-1>=0 && !visited[now[0]-1]) {
+                visited[now[0]-1]= true;
+                que.offer(new int[]{now[0]-1, now[1]+1});
+            }
+            if(now[0]+1 <=100000 && !visited[now[0]+1]) {
+                visited[now[0]+1]= true;
+                que.offer(new int[]{now[0]+1, now[1]+1});
+            }
+
+        }
+    }
+}
