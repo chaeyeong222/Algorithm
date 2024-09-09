d477b50148836b1b036b2aadb4845dec70320b25
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2024-09-09 15:55:42 +0900

    [Gold V] Title: 평범한 배낭, Time: 104 ms, Memory: 13404 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -3,7 +3,7 @@ import java.util.*;
 //배낭
 class Main {
     static int n, k;
-    static int[][] dp;
+    static int[] dp;
     static int[] value, weight;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,7 +12,7 @@ class Main {
         k = Integer.parseInt(st.nextToken());
         weight = new int[n+1];
         value = new int[n+1];
-        dp = new int[n+1][k+1];
+        dp = new int[k+1];
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             weight[i] = Integer.parseInt(st.nextToken());
@@ -20,16 +20,11 @@ class Main {
         }//입력
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= k; j++) {
-                if(weight[i] > j){
-                    dp[i][j] = dp[i-1][j];
-                }
-                else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-weight[i]]+value[i]);
-                }
+            for (int j = k; j-weight[i] >=0 ; j--) {
+                dp[j] = Math.max(dp[j], dp[j-weight[i]]+value[i]);
             }
         }
-        System.out.println(dp[n][k]);
+        System.out.println(dp[k]);
 
     }
 }
