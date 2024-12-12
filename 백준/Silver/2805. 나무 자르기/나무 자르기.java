aaaca82d60a998a349fb46945313c6451cd7c312
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2024-12-12 22:42:01 +0900

    [Silver II] Title: 나무 자르기, Time: 456 ms, Memory: 167804 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,39 +1,54 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+//##차이를 최대로
+class Main {
+    static int n, m, ans;
+    static int[] tree;
+    public static void main(String[] args) throws Exception {
+        input();
+        pro();
 
-public class Main {
-    public static void main(String[] args)throws Exception {
+    }
+    static void pro(){
+        //start 와 end 범위 안에 정답이 존재한다.
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        long m = Long.parseLong(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        long[] arr = new long[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Long.parseLong(st.nextToken());
-        }
-        Arrays.sort(arr);
-        long start = 1;
-        long end = arr[n-1];
-        long maxLength = 0;
+        long start = 0;
+        long end = 2000000000;
+        long ans = 0;
+
         while(start<=end){
-            long cnt = 0;
             long mid = (start+end)/2;
-            for (int i = 0; i < n; i++) {
-                long cut = arr[i]-mid;
-                if(cut>0) cnt+=cut;
-            }
-            if(cnt>=m) {
+            if(determination((int) mid)){
+                ans = mid;
                 start = mid+1;
-                maxLength = Math.max(maxLength, mid);
-            } else {
+            }else{
                 end = mid-1;
             }
         }
-        System.out.println(maxLength);
+        System.out.println(ans);
 
+
+    }
+
+    static boolean determination(int h){
+        //h높이로 나무를 잘랐을때, m만큼을 얻을 수 있으면 true, 아니면 false return
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            if(tree[i]>h) sum += (tree[i]-h);
+        }
+        return sum >= m;
+
+    }
+
+    static void input() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        tree = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            tree[i] = Integer.parseInt(st.nextToken());
+        }
     }
 }
