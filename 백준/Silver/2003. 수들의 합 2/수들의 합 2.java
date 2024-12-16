15056f2fbd8d66aad972def0b1266a6590be8fd9
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2024-12-16 17:12:18 +0900

    [Silver IV] Title: 수들의 합 2, Time: 96 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,35 +1,43 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] nums = new int[n+1];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
-        int cnt = 0;
-        int start = 0;
-        int end = 0;
-        int nowSum = 0;
-        while(end<=n){
-             if(nowSum>=m){
-                 nowSum-=nums[start++];
-             }else if(nowSum<m){
-                 nowSum+=nums[end++];
-             }
-             if(nowSum==m){
-                 cnt++;
-             }
-        }
-        System.out.println(cnt);
-
+import javax.swing.plaf.ProgressBarUI;
+import java.io.*;
+import java.util.*;
+//##
+class Main {
+    static int N,M;
+    static int[] num;
+    static int cnt;
+    public static void main(String[] args) throws Exception {
+        set();
+        pro();
     }
 
+    static void pro(){
+        long sum = 0;
+        int left = 0;
+        int right = 0;
+
+        while(right<=N){
+            if(sum >= M){
+                sum -= num[left++];
+            }else{
+                sum += num[right++];
+            }
+            if(sum==M) cnt++; //목적도달하면 cnt++)
+        }
+
+        System.out.println(cnt);
+    }
+
+    static void set() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        num = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        cnt = 0;
+    }
 }
