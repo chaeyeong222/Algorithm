6e999af8e48c2fa91858a84293fdbb5705078ddf
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2024-12-18 16:51:53 +0900

    [Gold IV] Title: 좋다, Time: 128 ms, Memory: 12812 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,43 +1,49 @@
 
-
 import java.io.*;
 import java.util.*;
-//##
+//## 13144
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] num = new int[n];
-        for (int i = 0; i < n; i++) {
-            num[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(num); //정렬
+    static int N;
+    static int[] num;
+    static int answer;
+    public static void main(String[] args) throws Exception {
+        set();
+        pro();
+    }
 
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            //i는 현재 체크해야할 값
+    static void pro(){
+        Arrays.sort(num);
+
+        for (int now = 0; now < N; now++) {
             int left = 0;
-            int right = n-1;
+            int right = N-1;
+            while(true) {
+                if (left == now) left++;
+                else if (right == now) right--;
 
-            while(true){
-                if(left==i){left++;}
-                else if(right==i){right--;}
+                if(left>=right) break;
 
-                if(left>=right)  break;
-
-                if(num[left]+num[right]>num[i]){
-                    right--;
-                }else if(num[left]+num[right]<num[i]){
+                if (num[left] + num[right] < num[now]) {
                     left++;
-                }else{
-                    cnt++;
+                } else if (num[left] + num[right] > num[now]) { 
+                    right--;
+                } else {
+                    answer++;
                     break;
                 }
             }
-        }//for
-        System.out.println(cnt);
-
+        }
+        System.out.println(answer);
     }
 
+    static void set() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        num = new int[N];
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        answer = 0;
+    }
 }
