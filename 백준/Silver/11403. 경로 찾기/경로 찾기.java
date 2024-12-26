bcb8e8001e58fe9d94ef2eceaee603669ac4e9cf
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2024-12-26 14:31:49 +0900

    [Silver I] Title: 경로 찾기, Time: 140 ms, Memory: 16772 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,42 +1,71 @@
+import com.sun.media.jfxmedia.events.VideoTrackSizeListener;
+
 import java.io.*;
 import java.util.*;
-public class Main {
+//## 11403
+class Main {
+    static int N;
+    static List<Integer>[] list;
+    static int[][] map;
+    static boolean[] visited;
+    static int[][] answer;
+    static StringBuilder sb;
+    public static void main(String[] args) throws Exception {
+        set();
+        pro();
+        System.out.println(sb);
+    }
+    public static void pro(){
 
-    public static void main(String[] args) throws IOException {
+        visited = new boolean[N];
+        for (int i = 0; i < N; i++) {
+            bfs(i);
+        }
+    }
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+    static void bfs(int start){
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(start);
+        for (int i = 0; i < N; i++) {
+            visited[i] = false; //다시 초기화
+        }
 
-        int n = Integer.parseInt(br.readLine());
-
-        int [][] board = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
+        while(!que.isEmpty()){
+            int now = que.poll();
+            for (int i = 0; i < N; i++) {
+                if(map[now][i]==0) continue;
+                if(visited[i]) continue;
+                que.offer(i);
+//                answer[now][i] = 1;
+                visited[i] = true;
             }
         }
 
+        for (int i = 0; i < N; i++) {
+            sb.append(visited[i] ?1:0).append(' ');
+        }
+        sb.append('\n');
+    }
 
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if(board[i][k]==1 && board[k][j]==1){
-                        board[i][j]=1;
-                    }
+    static void set() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        list = new List[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = new ArrayList<>();
+        }
+        map = new int[N][N];
+        answer = new int[N][N];
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j]==1) {
+                    list[i].add(j);
                 }
             }
-        }
-
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++) {
-                System.out.print(board[i][j]+" ");
-            }
-            System.out.println();
-        }
-            
-
-
+        }//for
+        sb = new StringBuilder();
     }
 }
