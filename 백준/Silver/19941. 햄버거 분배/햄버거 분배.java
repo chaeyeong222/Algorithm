99096621be17d860b586e4b7ea277ea69cefb15f
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2025-02-10 11:38:51 +0900

    [Silver III] Title: 햄버거 분배, Time: 68 ms, Memory: 11812 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -1,36 +1,38 @@
-
 import java.io.*;
 import java.util.*;
-//##19941 햄버거분배
-public class Main {
+class Main {
+    static int N, K, cnt;
+    static String line;
+
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        String temp = br.readLine();
-        char[] arr = temp.toCharArray();
-
-        boolean[] visited = new boolean[n];
-
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if(arr[i]=='P'){
-                for (int j = i-k; j <= i+k; j++) {
-                    if(j<0 || j>=n) continue;
-                    if(arr[j]=='H' && !visited[j]) {
+        set();
+        pro();
+        System.out.println(cnt);
+    }
+    public static void pro() {
+        char[] check = line.toCharArray();
+        boolean[] visited = new boolean[check.length];
+        for (int i = 0; i < check.length; i++) {
+            if(check[i]=='P'){
+                for (int j = i-K; j <= i+K; j++) {
+                    if(j>=0 && j<N && check[j]=='H' && !visited[j]){
                         cnt++;
                         visited[j] = true;
                         break;
                     }
-                }//
+                }
             }
         }
-        System.out.println(cnt);
 
 
+    }
 
-
-
-    }//main
+    static void set() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        line = br.readLine();
+        cnt = 0;
+    }
 }
