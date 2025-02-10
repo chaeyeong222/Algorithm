07b6ed0dd8ce37c8198a497b4b54ee4ec6a71de6
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2025-02-10 15:27:25 +0900

    [Gold V] Title: 문자열 게임 2, Time: 456 ms, Memory: 146732 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -1,48 +1,52 @@
-
 import java.io.*;
 import java.util.*;
-//##
-class Main {
-    static String word;
-    static int k;
-    public static void main(String[] args) throws IOException {
+
+public class Main  {
+    static char[] check;
+    static int K;
+    static String sentence;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int TC = 0; TC < T; TC++) {
-            word = br.readLine();
-            int[] value = new int['z'-'a'+1];
-            k = Integer.parseInt(br.readLine());
+            sentence = br.readLine();
+            check = sentence.toCharArray();
+            K = Integer.parseInt(br.readLine());
 
-            for (int i = 0; i < word.length(); i++) {
-                value[word.charAt(i)-'a']++;
-            }
-
-            int maxV = 0;
             int minV = Integer.MAX_VALUE;
-            for (int i = 0; i < word.length(); i++) {
-                char temp = word.charAt(i);
-                if(value[temp-'a']<k) continue;
-
-                String substr = checkWord(temp, i);
-
-                if(substr!=null){
-                    if(minV > substr.length()) minV = substr.length();
-                    if(maxV < substr.length()) maxV = substr.length();
-                }
+            int maxV = -1;
+            int[] value = new int['z'-'a'+1];
+            for (int i = 0; i < check.length ; i++) {
+                value[check[i]-'a']++;
             }
 
-            if(minV!=Integer.MAX_VALUE) sb.append(minV).append(' ').append(maxV).append('\n');
-            else sb.append(-1).append('\n');
-        }//TC
+            for (int i = 0; i < check.length; i++) {
+                char now = check[i];
+                if(value[now-'a'] < K) continue;
+                String temp = checkLen(now, i);
+
+                if(temp!=null){
+                    if(minV > temp.length()) minV =  temp.length();
+                    if(maxV < temp.length()) maxV =  temp.length();
+                }
+                
+            }
+            if(minV==Integer.MAX_VALUE) sb.append(-1);
+            else  sb.append(minV).append(' ').append(maxV);
+            
+            sb.append('\n');
+        }
         System.out.println(sb);
     }
-    public static String checkWord(char target, int start){
+    public static String checkLen(char pivot, int start){
+
         int cnt = 0;
-        for(int i=start; i<word.length(); i++){
-            if(word.charAt(i)==target) cnt++;
-            if(cnt==k) return word.substring(start,i+1);
+        for (int i = start; i < check.length; i++) {
+            if(check[i]==pivot) cnt++;
+            if(cnt==K) return sentence.substring(start, i+1);
         }
+
         return null;
     }
 }
