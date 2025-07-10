9f2baad1b0e155c4b740024a8e8b68235bf70a57
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2025-07-10 11:14:50 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 536 ms, Memory: 87540 KB -BaekjoonHub

--- 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.java
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.java
@@ -2,8 +2,7 @@ import java.io.*;
 import java.util.*;
 class Main {
     static int N;
-    static List<Integer>[] list;
-    static boolean[] visited;
+    static int[] parent;
     static int[] ans;
 
     public static void main(String[] args) throws Exception {
@@ -11,46 +10,43 @@ class Main {
         pro();
     }
     public static void pro() {
-        ans[0] = 1;
-        bfs(1);
         for (int i = 1; i <= N; i++) {
-            if(!visited[i]){
-                ans[1] = i;
-                break;
-            }
-        }
-        System.out.println(ans[0]+" "+ans[1]);
-    }
-    public static void bfs(int idx){
-        Queue<Integer> que = new LinkedList<>();
-        que.offer(idx);
-        visited[idx] = true;
-        while(!que.isEmpty()){
-            int now = que.poll();
-            for (int next : list[now]){
-                if(!visited[next]){
-                    que.offer(next);
-                    visited[next] = true;
+            for (int j = i + 1; j <= N; j++) {
+                if (find(i) != find(j)) {
+                    System.out.println(i + " " + j);
+                    return;
                 }
             }
         }
     }
+
     public static void set() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        visited = new boolean[N+1];
-        ans = new int[2];
-        list = new ArrayList[N+1];
-        for (int i = 0; i <= N; i++) {
-            list[i] = new ArrayList<>();
+        parent = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
         }
+        ans = new int[2];
         for (int i = 0; i < N-2 ; i++) {
             st =  new StringTokenizer(br.readLine());
             int num1 = Integer.parseInt(st.nextToken());
             int num2 = Integer.parseInt(st.nextToken());
-            list[num1].add(num2);
-            list[num2].add(num1);
+            union(num1, num2);
         }
+    }
+    public static void union(int a, int b){
+        int pA = find(a);
+        int pB = find(b);
+        if(pA!=pB){
+            parent[pB] = pA;
+        }
+    }
+    public static int find(int a){
+        if(parent[a] != a){
+            parent[a] = find(parent[a]);
+        }
+        return parent[a];
     }
 }
