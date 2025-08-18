d73041ef0c3bfee975f24a96515bf03b6d1c7c6c
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2025-08-18 12:31:12 +0900

    [Silver II] Title: DNA 비밀번호, Time: 180 ms, Memory: 20788 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,49 +1,46 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
+    static int N, M, len;
     static String word;
     static int[] need, now;
     static int answer;
-
     public static void main(String[] args) throws IOException {
         set();
         pro();
     }
-
-    public static void pro() { 
+    public static void pro(){
         for (int i = 0; i < M; i++) {
             char temp = word.charAt(i);
             now[findIdx(temp)]++;
         }
         check();
-
         int start = 0;
         int end = M;
-        while (end < word.length()) {
+        while(end<word.length()){
             now[findIdx(word.charAt(start++))]--;
             now[findIdx(word.charAt(end++))]++;
             check();
         }
-
         System.out.println(answer);
     }
-
-    public static void check() {
+    public static void check(){
         for (int i = 0; i < 4; i++) {
-            if (now[i] < need[i]) return;
+            if(now[i]<need[i]) return;
         }
         answer++;
     }
-
-    public static int findIdx(char c) {
-        switch (c) {
-            case 'A': return 0;
-            case 'C': return 1;
-            case 'G': return 2;
-            default: return 3; // 'T'
+    public static int findIdx(char c){
+        if(c=='A'){
+            return 0;
+        }else if(c=='C'){
+            return 1;
+        }else if(c=='G'){
+            return 2;
         }
+        return 3;
     }
 
     static void set() throws IOException {
@@ -52,11 +49,10 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         word = br.readLine();
-
+        len = word.length();
         need = new int[4];
         now = new int[4];
         answer = 0;
-
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
             need[i] = Integer.parseInt(st.nextToken());
