71d321d19ee278e2b99ea40b846e2ca38a9286cd
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2025-08-25 14:31:56 +0900

    [level 3] Title: 거스름돈, Time: 23.38 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -3,16 +3,13 @@ import java.io.*;
 
 class Solution {
     public int solution(int n, int[] money) {
-        int answer = 0;
-        int[] dp = new int[n+1];
-        for(int j=0; j<money.length; j++){
-            int nowCoin = money[j]; 
-            for(int i=1; i<=n ; i++){
-                if(i-nowCoin > 0){
-                    dp[i] = (dp[i]+dp[i-nowCoin])%1000000007;
-                }else if(i-nowCoin==0){
-                    dp[i]= (dp[i]+1)%1000000007;
-                }
+        int MOD = 1000000007;
+        int[] dp = new int[n + 1];
+        dp[0] = 1; // 아무 동전도 사용하지 않는 방법
+        
+        for (int coin : money) {
+            for (int i = coin; i <= n; i++) {
+                dp[i] = (dp[i] + dp[i - coin]) % MOD;
             }
         }
         return dp[n];
