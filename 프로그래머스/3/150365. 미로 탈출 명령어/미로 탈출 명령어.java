ecf5dbb44abe874b94a92fb3cbc73863d68149a6
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2025-10-08 21:20:18 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 2.11 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -3,6 +3,7 @@ import java.io.*;
 class Solution {
     public int[] dr={1,0,0,-1}; //d l r u 순서
     public int[] dc = {0,-1,1,0}; 
+    char[] dir = {'d','l','r','u'};
     public String answer = ""; 
     public int n,m,R,C;
     public String solution(int n, int m, int x, int y, int r, int c, int k) { 
@@ -10,42 +11,31 @@ class Solution {
         this.m=m;
         R=r-1;
         C=c-1;
-        escape(x-1,y-1,0,k,new StringBuilder());
-        if(answer.equals("")) return "impossible";
-        else return answer;
+        escape(x-1,y-1,k,new StringBuilder());
+        return answer.equals("")? answer ="impossible": answer;
     }
-    public void escape(int nowR, int nowC, int cnt, int k, StringBuilder sb){ 
-        if (!answer.equals("")) return;
-        if(sb.length()>k) return;//개수초과시 종료
-        if(sb.length()==k){
-            if(nowR == R && nowC ==C){//조건맞춤 
-                compare(answer, sb.toString()); 
-            }
-            return;
-        }
-        int dist = Math.abs(nowR-R) + Math.abs(nowC-C);
-        int remain = k-sb.length();
+    public void escape(int nowR, int nowC, int remain, StringBuilder sb){ 
+        if (!answer.equals("")) return; 
+        int dist = Math.abs(nowR-R) + Math.abs(nowC-C); 
         if(dist > remain ||  (remain-dist)%2==1){//남은 이동거리가 부족하거나 홀수이면
             return;            
         } 
+        if(remain==0){
+            if(nowR == R && nowC ==C){//조건맞춤 
+                answer = sb.toString();
+            }
+            return;
+        }
+        
         for(int i=0; i<4; i++){ 
             int nr = nowR + dr[i];
             int nc = nowC + dc[i];
             if(nr>=0 && nr<n && nc>=0 && nc<m ){ //범위안
-                if(i==0){   
-                    escape(nr, nc, cnt+1, k, new StringBuilder(sb).append('d'));
-                }else if(i==1){ 
-                    escape(nr, nc, cnt+1, k,  new StringBuilder(sb).append('l'));
-                }else if(i==2){
-                    escape(nr, nc, cnt+1, k, new StringBuilder(sb).append('r'));
-                }else if(i==3){
-                    escape(nr, nc, cnt+1, k,  new StringBuilder(sb).append('u'));
-                }
+                escape(nr, nc, remain-1, sb.append(dir[i]));
+                sb.deleteCharAt(sb.length()-1);
+                
+                 if (!answer.equals("")) return; 
             }
         }
-    }
-    public void compare(String a, String b){
-        if (answer.equals("") || b.compareTo(answer) < 0) answer = b;
-        return;
     }
 }
