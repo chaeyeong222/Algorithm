5e76f4a931bdb56e6c2abadd9df94e390b6f177d
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2025-10-14 17:03:32 +0900

    [Silver II] Title: 상자넣기, Time: 88 ms, Memory: 11892 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.java
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -1,31 +1,36 @@
 import java.io.*;
 import java.util.*;
-//##1965 상자넣기
-class Main {
-    public static Integer[] dp;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] box = new int[n];
-        for (int i = 0; i < n; i++) {
-            box[i] = Integer.parseInt(st.nextToken());
-        }
 
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                if(box[j] < box[i] && dp[j]+1 > dp[i]){
+public class Main {
+    static int N;
+    static int[] box;
+    public static void main(String[] args) throws IOException {
+        set();
+        pro();
+    }
+    public static void pro(){
+        int max = 0;
+        int[] dp = new int[N];
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if(box[j]<box[i] && dp[j]+1>dp[i]){
                     dp[i] = dp[j]+1;
                 }
+                max = Math.max(max, dp[i]);
             }
         }
-
-        int max = 1;
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, dp[i]);
-        }
         System.out.println(max);
+
+
+    }
+    public static void set() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        box = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            box[i] = Integer.parseInt(st.nextToken());
+        }
     }
 }
