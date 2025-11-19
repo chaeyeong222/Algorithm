682ea33fb27b0939ffd8286a9e1987ad9dd20398
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2025-11-19 15:07:48 +0900

    [Gold V] Title: 동전, Time: 68 ms, Memory: 11844 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.java
+++ 백준/Gold/9084. 동전/동전.java
@@ -1,33 +1,31 @@
 import java.io.*;
 import java.util.*;
-//##동전 9084
-class Main {
+
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        for (int TC = 0; TC < T; TC++) {
-            int n = Integer.parseInt(br.readLine());//가짓수
-            int[] coin = new int[n];
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < coin.length; i++) {
+        while(T-->0){
+            int N = Integer.parseInt(br.readLine());
+            int[] coin = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
                 coin[i] = Integer.parseInt(st.nextToken());
             }
-            int limit = Integer.parseInt(br.readLine());
-            int[] dp = new int[limit+1];
-            for (int i = 0; i < n; i++) {
-                int now = coin[i];
-                for (int j = 1; j <= limit; j++) {
-                    if(j-now > 0){
-                        dp[j] = dp[j]+dp[j-now];
-                    }else if(j-now==0){
-                        dp[j]++;
-                    }
+            int M = Integer.parseInt(br.readLine());
+            int[] dp = new int[M+1];
+            dp[0] = 1;
+            for (int i = 0; i < N; i++) {
+                int nowCoin = coin[i];
+                for (int j = nowCoin; j <= M; j++) {
+                    dp[j] += dp[j-nowCoin];
                 }
             }
-            sb.append(dp[limit]).append('\n');
+            sb.append(dp[M]).append('\n');
         }
         System.out.println(sb);
+
+
     }
 }
