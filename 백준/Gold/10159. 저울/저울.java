55d488ce6c6d7ddaab4bfbf870b9b8225ea119c7
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2025-12-24 11:49:32 +0900

    [Gold IV] Title: 저울, Time: 104 ms, Memory: 12960 KB -BaekjoonHub

--- 백준/Gold/10159. 저울/저울.java
+++ 백준/Gold/10159. 저울/저울.java
@@ -1,39 +1,58 @@
 import java.io.*;
-import java.util.*;
+import java.util.*; 
 
 public class Main {
     static int N, M;
-    static int[][] heavier;
-    public static void main(String[] args) throws IOException {
+    static List<Integer>[] list, reverse;
+    public static void main(String[] args) throws Exception {
+        set();
+        pro();
+    }
+    public static void pro(){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            int lighter = bfs(i, list);
+            int heavier = bfs(i, reverse);
+            sb.append(N-1-lighter-heavier).append('\n');
+        }
+        System.out.println(sb);
+    }
+    public static int bfs(int pivot, List<Integer>[] graph){
+        boolean[] visited = new boolean[N+1];
+        visited[pivot] = true;
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(pivot);
+        int cnt = 0;
+        while(!que.isEmpty()){
+            int now = que.poll(); 
+            for(int next: graph[now]){
+                if(!visited[next]) {
+                    visited[next] = true;
+                    que.offer(next);
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+
+    public static void set() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        heavier = new int[N+1][N+1];
+        list = new ArrayList[N+1];
+        reverse = new ArrayList[N+1];
+        for (int i = 1; i <= N; i++) {
+            list[i] = new ArrayList<>();
+            reverse[i] = new ArrayList<>();
+        }
         StringTokenizer st;
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            heavier[a][b] = 1;
+            list[a].add(b);
+            reverse[b].add(a);
         }
-        for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    if(heavier[i][k]==1 && heavier[k][j]==1){
-                        heavier[i][j] = 1;
-                    }
-                }
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            int cnt = 0;
-            for (int j = 1; j <= N; j++) {
-                if(i==j) continue;
-                if(heavier[i][j]==1 || heavier[j][i]==1) cnt++;
-            }
-            sb.append(N-cnt-1).append('\n');
-        }
-        System.out.println(sb);
     }
 }
