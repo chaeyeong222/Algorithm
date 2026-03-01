c398e9e67d36a18f8708670bdc11551c61e262bb
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2026-03-01 14:42:58 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 58.44 ms, Memory: 207 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -3,38 +3,47 @@ import java.io.*;
 class Solution {
     public int solution(int[][] board, int[][] skill) {
         int answer = 0;
-        int N = board.length;
-        int M = board[0].length;
-        int[][] change = new int[N+1][M+1];
-        for(int[] sk : skill){
-            int type = sk[0];
-            int r1 = sk[1];
-            int c1 = sk[2];
-            int r2 = sk[3];
-            int c2 = sk[4];
-            int power = type==1? sk[5]*(-1) : sk[5];  
-            change[r1][c1]+=power;
-            change[r1][c2+1]-=power;
-            change[r2+1][c1]-=power;
-            change[r2+1][c2+1]+=power;
-        }
-        for(int i=1; i<N; i++){
-            for(int j=0; j<M; j++){
-                change[i][j] += change[i-1][j];
-            }
-        }
-        
-        for(int i=0; i<N; i++){
-            for(int j=1; j<M; j++){ 
-                change[i][j] += change[i][j-1];
+        int n = board.length;
+        int m = board[0].length;
+        int[][] prev = new int[n+1][m+1];
+        for(int[] s : skill){
+            int type = s[0];
+            int r1 = s[1];
+            int c1 = s[2];
+            int r2 = s[3];
+            int c2 = s[4];
+            int degree = s[5];
+            if(type==1){ //공격
+                prev[r1][c1] -= degree;
+                prev[r1][c2+1] += degree;
+                prev[r2+1][c1] += degree;
+                prev[r2+1][c2+1] -= degree;
+            }else{
+                prev[r1][c1] += degree;
+                prev[r1][c2+1] -= degree;
+                prev[r2+1][c1] -= degree;
+                prev[r2+1][c2+1] += degree;
             }
         } 
-        
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(board[i][j]+change[i][j]>0) answer++;
+        for(int i=0; i<n; i++){
+            for(int j= 1; j<m; j++){
+                prev[i][j] += prev[i][j-1]; 
             }
         }
+        
+        for(int i=0; i<m; i++){
+            for(int j= 1; j<n; j++){
+                prev[j][i] += prev[j-1][i]; 
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j]+prev[i][j]>0) answer++;
+            }
+        }
+        
+        
         return answer;
     }
 }
