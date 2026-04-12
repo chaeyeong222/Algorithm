9a9380ee14da62fce1f9d2d5ca496e3dfc789af1
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2026-04-12 16:04:08 +0900

    [level 2] Title: 힌트 스테이지, Time: 10.67 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.java
+++ 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.java
@@ -18,21 +18,21 @@ class Solution {
             minCost = Math.min(minCost, money);
             return;
         }
+        if(money>=minCost) return;
         
-        //힌트 안사는 경우 > 이미 가지고 있는 힌트개수로만 처리
-        int[] copy = nowHints.clone();
+        //힌트 안사는 경우 > 이미 가지고 있는 힌트개수로만 처리 
         int hintCnt = Math.min(nowHints[depth], n-1); //가지고 있는 힌트 개수
-        solve(depth+1, copy, money+cost[depth][hintCnt]);
+        solve(depth+1, nowHints.clone(), money+cost[depth][hintCnt]);
         
         //힌트 사는 경우
-        int[] copy2 = nowHints.clone();
+        int[] next = nowHints.clone();
         int hintCost = hint[depth][0]; //힌트비용
         for(int i=1; i<hint[depth].length; i++){
             int t = hint[depth][i];
-            copy2[t-1]++; 
+            next[t-1]++; 
         }
-        int hintCnt2 = Math.min(copy2[depth], n-1); //가지고 있는 힌트 개수
-        solve(depth+1, copy2, money+hintCost+cost[depth][hintCnt2]);
+        int hintCnt2 = Math.min(next[depth], n-1); //가지고 있는 힌트 개수
+        solve(depth+1, next, money+hintCost+cost[depth][hintCnt2]);
         
     }
 } 
