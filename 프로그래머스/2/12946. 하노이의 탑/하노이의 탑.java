1b0c50272bf0e958a00c33e43bc4c6d14e0be10f
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2026-08-01 13:57:45 +0900

    [level 2] Title: 하노이의 탑, Time: 3.33 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,26 +1,26 @@
 import java.util.*;
-import java.io.*;
 class Solution {
+    int turn = 0;
     List<int[]> list;
-    int m=0;
     public int[][] solution(int n) {
         list = new ArrayList<>();
-        hanoi(n,1,3,2);
-        int[][] answer = new int[m][2];
-        for(int i=0; i<m; i++){
-            answer[i] = list.get(i); 
+        hanoi(n, 1, 3, 2);
+        int[][] answer = new int[turn][2];
+        for(int i=0; i<turn; i++){
+            answer[i][0] = list.get(i)[0];
+            answer[i][1] = list.get(i)[1];
         }
         return answer;
     }
     public void hanoi(int n, int from, int to, int via){
         if(n==1){
-            m++;
-            list.add(new int[]{from,to});
+            turn++;
+            list.add(new int[]{from,to});//가장 큰 원반 옮기기
             return;
         }
-        hanoi(n-1, from, via, to);
-        m++;
-        list.add(new int[]{from, to});
-        hanoi(n-1, via, to, from);  
+        hanoi(n-1, from, via, to); //n-1을 via 로 옮기기
+        turn++;
+        list.add(new int[]{from,to}); //
+        hanoi(n-1, via, to, from); // via에 있는 것들을 to 로 옮기기
     }
 }
