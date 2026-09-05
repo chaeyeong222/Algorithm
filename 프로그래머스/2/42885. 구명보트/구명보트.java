ebfb1ce73ca6b1257402b418a101d11403d6fe18
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2026-09-05 21:54:44 +0900

    [level 2] Title: 구명보트, Time: 11.60 ms, Memory: 59.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,19 +1,20 @@
 import java.util.*;
-import java.io.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        Arrays.sort(people);//정렬
+        Arrays.sort(people);
+        int prev = people[0];
+        int n = people.length;
         int left = 0;
-        int right = people.length-1;
+        int right = n-1;
         while(left<=right){
-            if((people[left]+people[right])>limit){
-                answer++;
+            if(people[left]+people[right]>limit){
                 right--;
+                answer++;
             }else{
                 answer++;
                 left++;
-                right--;
+                right--; 
             }
         }
         return answer;
