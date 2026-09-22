18cb9fb55780c3709f0be920e91389ef12b1ea8a
chaeyeong222 <91577661+chaeyeong222@users.noreply.github.com>
2026-09-22 22:04:47 +0900

    [level 3] Title: 입국심사, Time: 34.69 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,27 +1,33 @@
 import java.util.*;
-import java.io.*;
-
 class Solution {
+    int n;
+    int[] times;
     public long solution(int n, int[] times) {
         long answer = 0;
+        long left = 0;
+        this.n = n;
+        this.times = times;
         Arrays.sort(times);
-        long start = times[0];
-        long end = (long) times[times.length-1]*n;
-        long mid; 
-        while(start <= end){
-            long sum = 0;
-            mid = (long)((start+end)/2);
-            for(int i:times){
-                sum += (mid/i);
-            } 
-            
-            if(sum >= n){
-                end = mid-1;
+        long right = (long) n * times[times.length-1];
+        while(left<=right){
+            long mid = (left+right)/2;
+            if(check(mid)){
                 answer = mid;
+                right = mid-1;
             }else{
-                start = mid+1;
-            } 
+                left = mid+1;
+            }
         }
         return answer;
+    }
+    public boolean check(long pivot){
+        long cnt = 0 ;
+        for(int i=0; i<times.length; i++){
+            cnt += pivot/times[i];
+            
+            
+            if(cnt>=n) return true;
+        } 
+        return false;
     }
 }
